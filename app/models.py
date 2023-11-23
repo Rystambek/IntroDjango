d12d2441be9a20b6a11ffadeec523f0b3f60e8c1
Rystambek <rustambekjumaqulov@gmail.com>
2023-11-24 01:36:24 +0500

    car

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=80)
+    price = models.FloatField()
+    color = models.CharField(max_length=80)
+    model = models.CharField(max_length=80)
+    years = models.IntegerField()
+    motors = models.CharField(max_length=80)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
