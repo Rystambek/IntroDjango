6901863ecb1cf2ae24d846e6ffa33d7fe18ce641
Rystambek <rustambekjumaqulov@gmail.com>
2023-11-24 22:17:59 +0500

    car update

--- app/urls.py
+++ app/urls.py
@@ -6,10 +6,20 @@ from .views import (
             index,
             get_num,
             get_sum,
-            get_user
+            get_user,
+            cars_all,
+            add_car,
+            car_id,
+            car_del,
+            car_upd
         )
 
 urlpatterns = [
+    path('cars-all/',cars_all),
+    path('add-car/',add_car),
+    path('car-id/<int:id>',car_id),
+    path('car-del/<int:id>',car_del),
+    path('car-upd/<int:id>',car_upd),
     path('',index),
     path('home/',home),
     path('get-num/',get_num),
